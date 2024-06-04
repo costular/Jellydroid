bc4e5e07acbe3ff66182d3dfefe8501911ce1ecb
Diego Francisco Concepción <costular@gmail.com>
2024-06-04 11:49:34 +0100

    WIP Initial set up

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.costular.jellybean.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
+    }
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.android.tools.common)
+    compileOnly(libs.compose.gradlePlugin)
+    compileOnly(libs.firebase.crashlytics.gradlePlugin)
+    compileOnly(libs.firebase.performance.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.room.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Jellybean"
 include(":app")
- 
+include(":logging")
